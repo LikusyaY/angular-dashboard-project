13132ab88fe33677965bd5dad5c967dcae29e369
LikusyaY <localhost@localho.st>
2025-09-08 18:30:41 +0300

    Компонент детальной страницы объявления

---
+++ src/app/pages/ad-page/ad-page.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-ad-page',
+  imports: [],
+  templateUrl: './ad-page.component.html',
+  styleUrl: './ad-page.component.scss'
+})
+export class AdPageComponent {
+
+}
