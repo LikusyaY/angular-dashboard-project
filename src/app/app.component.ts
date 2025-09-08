dbd6b677f0673cfd1b4fcb34c125ead4dcaf0d01
LikusyaY <localhost@localho.st>
2025-09-08 18:31:25 +0300

    Обновлен главный компонент

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+// import { HeaderComponent } from "./components/header/header.component";
+// import { AdsListComponent } from "./components/ads-list/ads-list.component";
+import { HomeComponent } from "./pages/home/home.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HomeComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
