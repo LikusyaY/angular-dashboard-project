5226dc3fa7c5bd8d1c727f3638f64c9d9be414b6
LikusyaY <localhost@localho.st>
2025-09-10 20:06:10 +0300

    Добавлен конфиг для prettier, обновлне тема primeng

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,7 +3,7 @@ import { provideRouter } from '@angular/router';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { providePrimeNG } from 'primeng/config';
 
-import Aura from '@primeng/themes/aura';
+import Lara from '@primeng/themes/lara';
 
 import { routes } from './app.routes';
 
@@ -13,7 +13,7 @@ export const appConfig: ApplicationConfig = {
     provideAnimationsAsync(),
     providePrimeNG({
       theme: {
-        preset: Aura
+        preset: Lara
         }
     })
   ]
